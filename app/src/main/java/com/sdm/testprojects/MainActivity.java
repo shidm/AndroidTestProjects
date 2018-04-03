b6eb11e6fb831daef48d573e4bbacb2a7467b9bd
shidongming <shidongming@meizu.com>
2018-04-03 16:36:06 +0800

    new test

--- app/src/main/java/com/sdm/testprojects/MainActivity.java
+++ app/src/main/java/com/sdm/testprojects/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Build;
 import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -74,6 +75,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lambda.setOnClickListener(this);
         permission.setOnClickListener(this);
         socket.setOnClickListener(this);
+
     }
 
     @Override
